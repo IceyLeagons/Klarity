311b32a2f5198272a6eb956a83f33decfb5b68b3
TOTHTOMI <TOTHTOMI57@hotmail.hu>
2023-07-23 18:01:47 +0200

    :construction_worker: Added maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.21"
     id("org.jetbrains.dokka") version "1.8.20"
+    `maven-publish`
     application
 }
 
