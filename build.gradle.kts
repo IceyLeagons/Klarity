bd5c67c2caf7a409631af1c8f2002a7b97f36821
TOTHTOMI <TOTHTOMI57@hotmail.hu>
2023-12-28 18:57:40 +0100

    NPM Readme
    Bumped version due to NPM package update!

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "net.iceyleagons"
-version = "2.0.1"
+version = "2.0.2"
 
 repositories {
     mavenCentral()
@@ -74,7 +74,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "net.iceyleagons"
             artifactId = "klarity"
-            version = "2.0.1"
+            version = "2.0.2"
 
             from(components["java"])
         }
