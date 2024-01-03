5af4546b44f35f99a084ba1e8ce7bcf58146b90e
TOTHTOMI <TOTHTOMI57@hotmail.hu>
2024-01-03 11:44:19 +0100

    :green_heart: hopefully fixed jitpack build

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 kotlin {
     jvm {
         compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.jvmTarget = "11"
         }
         withJava()
         testRuns["test"].executionTask.configure {
